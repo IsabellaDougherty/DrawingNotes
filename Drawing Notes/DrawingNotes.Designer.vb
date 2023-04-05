e25b71eaf5f55f98328d05ee963542f44f5fa09d
Isabella <belladougherty@gmail.com>
2023-04-05 09:02:27 -0600

    Got code in and running

---
+++ Drawing Notes/DrawingNotes.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ContentMenuStripToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChooseColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ContentMenuStripToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ContentMenuStripToolStripMenuItem
+        '
+        Me.ContentMenuStripToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChooseColorToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContentMenuStripToolStripMenuItem.Name = "ContentMenuStripToolStripMenuItem"
+        Me.ContentMenuStripToolStripMenuItem.Size = New System.Drawing.Size(117, 20)
+        Me.ContentMenuStripToolStripMenuItem.Text = "ContentMenuStrip"
+        '
+        'ChooseColorToolStripMenuItem
+        '
+        Me.ChooseColorToolStripMenuItem.Name = "ChooseColorToolStripMenuItem"
+        Me.ChooseColorToolStripMenuItem.Size = New System.Drawing.Size(146, 22)
+        Me.ChooseColorToolStripMenuItem.Text = "Choose Color"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(776, 411)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ContentMenuStripToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChooseColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ Drawing Notes/DrawingNotes.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Dim currentColor As Color
+    Sub testDraw()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim myPen As Pen = New Pen(Me.currentColor)
+
+        g.DrawLine(myPen, 0, 0, 500, 100)
+        myPen.Color = Color.Green
+        myPen.Width = 10
+        g.DrawEllipse(myPen, 200, 200, 400, 400)
+        'g.FillEllipse(mybrush, 300, 300, 350, 350)
+
+
+
+        g.Dispose()
+        myPen.Dispose()
+    End Sub
+    Sub DrawLineSegment(x1%, y1%, x2%, y2%)
+        Dim g As Graphics = PictureBox1.CreateGraphics
+        Dim myPen = New Pen(Me.currentColor)
+
+        g.DrawLine(myPen, x1, y1, x2, y2)
+
+        myPen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub UpdateColor()
+        ColorDialog1.ShowDialog()
+        Me.currentColor = ColorDialog1.Color
+    End Sub
+    Sub Clear()
+        PictureBox1.Refresh()
+        'PictureBox.BackColor = Color.White
+    End Sub
+
+    ' event handlers
+
+    Private Sub GraphicsForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        currentColor = Color.Black
+    End Sub
+
+    Private Sub GraphicsForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        'testDraw()
+    End Sub
+
+    Private Sub PictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove, PictureBox1.MouseDown
+        Static lastX%, lastY%
+        Select Case e.Button.ToString
+            Case "Left"
+                DrawLineSegment(lastX, lastY, e.X, e.Y)
+            Case "Middle"
+                UpdateColor()
+        End Select
+        Me.Text = $"({e.X},{e.Y}) button: {e.Button.ToString} Color: {Me.currentColor.ToString}"
+        lastX = e.X
+        lastY = e.Y
+    End Sub
+
+    Private Sub ChooseColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChooseColorToolStripMenuItem.Click
+        UpdateColor()
+    End Sub
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        Clear()
+    End Sub
+End Class
